2195c6e8dfe17524d7f6460ce54c7204ec256b32
JairEsc <p.escobar816@gmail.com>
2025-09-20 14:43:52 -0600

    version preliminar post-seleccion

--- Rproj/Codigos/pca_post.R
+++ Rproj/Codigos/pca_post.R
@@ -1,51 +1,38 @@
 ###Hacmos el pca con las variables seleccionadas
 
-pca_indicadores_post=prcomp(indicadores[,-1], scale = TRUE)
+pca_indicadores_post=prcomp(indicadores[,-1], scale = TRUE,retx = T)
 summary(pca_indicadores_post)
 ###Con 9 componentes se tiene para cubrir >70%
-####Con la otra librería 
-factoextra::fviz_pca_var(pca_indicadores_post, col.var = "black")
-
-slice_top5=function(col,top){
-  
-  #Buscar esos indicadores en el diccionario y regresar sus significados
-  return(diccionario_variables[diccionario_variables$variables%in%(pca_indicadores_full$rotation[pca_indicadores_full$rotation[,col] |> abs() |> order(decreasing = T),col])[1:col,top] |> names(),])
-}
-
-(pca_indicadores_post$rotation[pca_indicadores_full$rotation[,1] |> abs() |> order(decreasing = T),1])[1:5] |> names() |> sapply(\(z){z==diccionario_variables$variables},simplify = T,USE.NAMES = F) |> which()
-##Componente 1: 
+ 
 pca_indicadores_post$rotation[pca_indicadores_post$rotation[,1] |> abs() |> order(decreasing = T),1] |> abs() |> plot()
-pca_indicadores_post$rotation[pca_indicadores_post$rotation[,1] |> abs() |> order(decreasing = T),1]##Cortamos a 12
+pca_indicadores_post$rotation[pca_indicadores_post$rotation[,1] |> abs() |> order(decreasing = T),1]##Cortamos a 2
 ####-----------
 # ingresos,  viviendas sin internet, marginacion, personas sin educacion basica, 
 #ingresos menores a la linea de pobreza, 
 #poblacion desocupada, pib estatal, 
 #Resumen: pobreza por ingresos. 
 ##Componente 2: 
-pca_indicadores_full$rotation[pca_indicadores_full$rotation[,2] |> abs() |> order(decreasing = T),2] |> abs()|> plot()
-pca_indicadores_full$rotation[pca_indicadores_full$rotation[,2] |> abs() |> order(decreasing = T),2]#Cortamos a 8
+pca_indicadores_post$rotation[pca_indicadores_post$rotation[,2] |> abs() |> order(decreasing = T),2] |> abs()|> plot()
+pca_indicadores_post$rotation[pca_indicadores_post$rotation[,2] |> abs() |> order(decreasing = T),2]#Cortamos a 8
 slice_top5(3)
 # + carencia alimentaria, - poco personal de seguridad, - alumbrado publico, 
 # - pob discapacitada,  
 #carencia acceso vivienda, baja inversion a carreteras, poblacion inactiva,tiendas liconsa, mucha enf_intestinales.
 #Resumen: Medidad de seguridad y carencia alimentaria. 
 ##Componente 3: 
-pca_indicadores_full$rotation[pca_indicadores_full$rotation[,3] |> abs() |> order(decreasing = T),3]|> abs()|> plot()
-pca_indicadores_full$rotation[pca_indicadores_full$rotation[,3] |> abs() |> order(decreasing = T),3] #Cortamos a 4
+pca_indicadores_post$rotation[pca_indicadores_post$rotation[,3] |> abs() |> order(decreasing = T),3] |> abs()|> plot()
+pca_indicadores_post$rotation[pca_indicadores_post$rotation[,3] |> abs() |> order(decreasing = T),3]
 #+ presencia de delitos, + participacion al pib, +inversion en escuelas, + poblacion desocupada
 #- poblacion sin derechohabiencia
 #, poca poblacion desocupada, pocas personas inactivas,
 #Poco personal de seguridad, muchas superficies siniestradas, baja inversion en carreteras
 #Resumen. Baja presencia de delitos, más participacion al pib
 ##Componente 4: 
-pca_indicadores_full$rotation[pca_indicadores_full$rotation[,4] |> abs() |> order(decreasing = T),4]|> abs()|> plot()
-pca_indicadores_full$rotation[pca_indicadores_full$rotation[,4] |> abs() |> order(decreasing = T),4] #Cortamos a 4
+pca_indicadores_post$rotation[pca_indicadores_post$rotation[,4] |> abs() |> order(decreasing = T),4]|> abs()|> plot()
+pca_indicadores_post$rotation[pca_indicadores_post$rotation[,4] |> abs() |> order(decreasing = T),4] #Cortamos a 4
 ## - kilometros de carreteras, + unidades economicas, - areas verdes , + superficies de cosecha siniestradas
 
 #Resumen. 
-
-
-
 
 
 
--- Rproj/Codigos/post_seleccion_vars.R
+++ Rproj/Codigos/post_seleccion_vars.R
@@ -1,4 +1,4 @@
-"../Datos/input.xlsx" |> openxlsx::read.xlsx()->indicadores
+#"../Datos/input.xlsx" |> openxlsx::read.xlsx()->indicadores
 
 
 ###Iniciamos con 37 variables. De las cuales ya identificamos algunas que son altamente correlacionadas y pertenecen al mismo tema. 
@@ -8,7 +8,6 @@
 #marginacion y porcentaje de personas mayores a 15 años sin educacion básica.
 # porcentaje de viviendas sin accesoa  inernet. 
 #Dado que muchas de ellas están involucradas en el cálculo de la marginación, la consideramos una medida resumen y la conservamos. 
-matrix_corr=cor(indicadores[,-1])
 indicadores=indicadores |> dplyr::select(-ind_ingreso,-PORC_POB_IILP,-pje_pob_15ymas_edbasinc_2020,-Pje_vivnodinter_2020)
 
 
@@ -16,5 +15,6 @@ indicadores=indicadores |> dplyr::select(-ind_ingreso,-PORC_POB_IILP,-pje_pob_15
 #población desocupada. Carpetas puestas a disposición de fiscalía, y pib estatal. 
 #Por otro lado, existe correlación alta entre delitos del fuero comun  carpetas ... 
 #Eliminamos la de carpetas. 
-indicadores=indicadores |> dplyr::select(-p_disp_seg)
+indicadores=indicadores |> dplyr::select(-p_disp_seg,-Pob_des)
 
+matrix_corr=cor(indicadores[,-1])
